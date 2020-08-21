a5b1e4dc9b6b08c6d98763091d7e091e0d1bb48d
RED-VIROUS7 <68949718+RED-VIROUS7@users.noreply.github.com>
2020-08-21 19:50:34 +0200

    Add files via upload

---
+++ commands.sh
@@ -0,0 +1,60 @@
+apt update && apt upgrade -y
+sleep 2
+pkg update -y
+sleep 2
+pkg upgrade -y
+sleep 2
+pkg install nano -y
+sleep 2
+pkg install php -y
+sleep 2
+pkg install figlet -y
+sleep 2
+pkg install cmatrix -y
+sleep 2
+pkg install python -y
+sleep 2
+plg install python2 -y
+sleep 2
+pkg install wget proot -y
+sleep 2
+pkg install git curl -y
+sleep 2
+pkg install man -y
+sleep 2
+pkg install util-linux -y
+sleep 2
+pkg install x11-repo -y
+sleep 2
+pkg install root-repo -y
+sleep 2
+pkg install unstable-repo -y
+sleep 2
+pkg install metasploit -y
+sleep 2
+pkg install pip -y
+sleep 2
+pkg install pip2 -y
+sleep 2
+pkg install openssh -y
+sleep 2
+pkg install openssh-server -y
+sleep 2
+pkg install ssh -y
+sleep 2
+pkg install unzip -y
+sleep 2
+pkg install unrar -y
+sleep 2
+pkg install vlang
+sleep 2
+pkg install hydra
+sleep 2
+pkg install nmap
+sleep 2
+pkg install wireshark
+sleep 2
+pip2 install update
+sleep 5
+echo "SEE YOU SOON"
+exit
